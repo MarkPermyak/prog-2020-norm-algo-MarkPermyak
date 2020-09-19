07e622dd1e0fe1612b290f81dd368fbfd8ad9e8f
markd <mark.permyakov.2002@bk.ru>
2020-09-19 14:58:11 +0300

    new feature:  now substrings can be an empty word ('0')

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-k=0
+k = 0
 with open('sample.txt') as file:
     d = dict()
     for line in file.readlines():
-        d[k]=line.rstrip().split(' -> ');
-        k+=1
+        d[k] = line.rstrip().split(' -> ')
+        k += 1
 
-g=open('intr.txt','r')
-s=g.readline()
+g = open('intr.txt', 'r')
+s = g.readline()
 
-def change(s,i):
-    s1=d[i][0]
-    num=s.find(s1)
-    if (num>-1):
+
+def change(s, i):
+    s1 = d[i][0]
+    num = s.find(s1)
+    if s1 == '0':
+        num = 0
+    if num > -1:
         l = len(s1)
         sn = s + 'e'
-        s = sn[0:num] + d[i][1] + sn[num+l:-1]
-    return(s)
-for j in range (0,k):
-    s=change(s,j)
+        if d[i][1] != '0':
+           s = sn[0:num] + d[i][1] + sn[num+l:-1]
+        else:
+           s = sn[0:num] + sn[num+l:-1]
+    return s
+
+
+for j in range(0, k):
+    s = change(s, j)
+
+print(s)
