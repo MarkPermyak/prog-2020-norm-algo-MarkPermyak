f0deb8e2875bf4b9d82bbc11906848d5cd7a4f9e
markd <mark.permyakov.2002@bk.ru>
2020-09-19 18:28:58 +0300

    some fixes

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ k = 0
 with open('sample.txt') as file:
     d = dict()
     for line in file.readlines():
-        d[k] = line.rstrip().split(' -> ')
+        if line.find(' ->. ') != -1:
+            d[k] = line.rstrip().split(' ->. ')
+        else:
+            d[k] = line.rstrip().split(' -> ')
         k += 1
 
 g = open('intr.txt', 'r')
@@ -15,15 +18,17 @@ def change(s, i):
     if s1 == '0':
         num = 0
     if num > -1:
-        l = len(s1)
+        if s1 == '0':
+            l = 0
+        else:
+            l = len(s1)
         sn = s + 'e'
         if d[i][1] != '0':
             s = sn[0:num] + d[i][1] + sn[num+l:-1]
         else:
-            if num != 0:
+
                 s = sn[0:num] + sn[num+l:-1]
-            else:
-                s = s
+
     return s
 
 
